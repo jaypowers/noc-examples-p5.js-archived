91523501046df5e7a899e88334c74696b9413625
jaypowers <japow75@gmail.com>
2025-07-01 14:53:26 -0400

    Big happy accident, added color and movement

--- chp00_introduction/Noise 2D_0.0.1/sketch.js
+++ chp00_introduction/Noise 2D_0.0.1/sketch.js
@@ -1,6 +1,10 @@
 // The Nature of Code
 // Daniel Shiffman
 // http://natureofcode.com
+let bright;
+let mycolor;
+let xoff = 0.0; // x-offset for perlin noise
+let yoff = 0.0; // y-offset for perlin noise
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -10,20 +14,21 @@ function setup() {
 function draw() {
   // Tell p5 we will work with pixels
   loadPixels();
-  let xoff = 0.0;
 
   // Updating pixels with perlin noise
   for (let x = 0; x < width; x++) {
-    let yoff = 0.0;
+    yoff = 0.0;
 
     for (let y = 0; y < height; y++) {
       // Calculating brightness value for noise
-      let bright = map(noise(xoff, yoff), 0, 1, 0, 255);
-      set(x, y, floor(bright));
+      bright = map(noise(xoff, yoff), 0, 1, 0, 255);
+      mycolor = color(bright, 100, 150); // Creating a color with brightness
+      set(x, y, mycolor);
       yoff += 0.01; // Incrementing y-offset perlins noise
     }
     xoff += 0.01; // Incrementing x-offset perlins noise
   }
 
   updatePixels();
+  xoff += 0.01; // Incrementing x-offset perlins noise for next frame
 }
