e5548db828cc140c4befaf267daf4281949534fc
jaypowers <japow75@gmail.com>
2025-07-01 14:09:38 -0400

    Added walker class

--- chp00_introduction/NOC_I_01_RandomWalkTraditional/sketch.js
+++ chp00_introduction/NOC_I_01_RandomWalkTraditional/sketch.js
@@ -14,30 +14,3 @@ function draw() {
   walker.step();
   walker.render();
 }
-
-class Walker {
-  constructor() {
-    this.x = width / 2;
-    this.y = height / 2;
-  }
-
-  render() {
-    stroke(0);
-    point(this.x, this.y);
-  }
-
-  step() {
-    let choice = floor(random(4));
-    if (choice === 0) {
-      this.x++;
-    } else if (choice == 1) {
-      this.x--;
-    } else if (choice == 2) {
-      this.y++;
-    } else {
-      this.y--;
-    }
-    this.x = constrain(this.x, 0, width - 1);
-    this.y = constrain(this.y, 0, height - 1);
-  }
-}
